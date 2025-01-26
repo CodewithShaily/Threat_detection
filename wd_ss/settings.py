6cf7f22a87c57ea51505365f6a8c765b0b9adb4b
CodewithShaily <167101198+CodewithShaily@users.noreply.github.com>
2025-01-26 08:55:47 +0530

    Update settings.py

--- wd_ss/settings.py
+++ wd_ss/settings.py
@@ -186,7 +186,7 @@ AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
 
 AWS_STORAGE_BUCKET_NAME = 'uploaded-alerts'
-AWS_S3_CUSTOM_DOMAIN = 'https://nagarrakshak.s3.eu-north-1.amazonaws.com/media/'
+AWS_S3_CUSTOM_DOMAIN ='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
